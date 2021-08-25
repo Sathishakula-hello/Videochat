a11329ab2f90dce449f861b89d00047ef4db85f2
unknown <akulasathish2001@gmail.com>
2021-08-26 00:13:55 +0530

    i

--- views/app.js
+++ views/app.js
@@ -10,7 +10,7 @@ do{
 }while(!name1);
 const video_grid=document.querySelector(".video_grid");
     //const peer=new Peer(undefined,{host:"/",port:3001})
-    const peer = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443})
+    const peer = new Peer(undefined,{host:'peerjs-server.herokuapp.com', secure:true, port:443})
     peer.on("open",(id)=>
     {
         our_user_id=id;
